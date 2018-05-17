d971cc9dd879bf9e207a89fc643f33c2239dc673
dirk wiggins <dirk@echo19.com>
2018-05-17 00:38:22 -0400

    model and passport updates

--- public/assets/js/new_members.js
+++ public/assets/js/new_members.js
@@ -4,10 +4,6 @@ $(document).ready(function () {
     $("#newMemberForm").on("submit", function (event) {
         console.log("success")
         event.preventDefault();
-        // if ($.trim($("#userName").val()) === "" || $.trim($("#userName").val()) === "User Name" || $.trim($("#userName").val()) === "Please enter a User Name") {
-        //     $("#userName").val(" Please enter a User Name");
-        //     return false;
-        // }
         if ($.trim($("#password").val()) === "" || $.trim($("#password").val()) === "Password" || $.trim($("#password").val()) === "Please enter a Password") {
             $("#password").val(" Please enter a Password");
             return false;
@@ -32,29 +28,28 @@ $(document).ready(function () {
             $("#photoUrl").val(" Please enter the URL address of your photo");
             return false;
         }
-        // if ($("#selectFrequency").val() === "" || $("#selectFrequency").val() === "Choose..." || $("#selectFrequency").val() === "Please select an option") {
-        //     $("#selectFrequency").val(" Please select an option");
-        //     return false;
-        // }
-        // if ($("#selectInOrOut").val() === "" || $("#selectInOrOut").val() === "Choose..." || $("#selectInOrOut").val() === "Please select an option") {
-        //     $("#selectInOrOut").val(" Please select an option");
-        //     return false;
-        // } 
+        if ($("#selectFrequency").val() === "" || $("#selectFrequency").val() === "Choose..." || $("#selectFrequency").val() === "Please select an option") {
+            $("#selectFrequency").val(" Please select an option");
+            return false;
+        }
+        if ($("#selectInOrOut").val() === "" || $("#selectInOrOut").val() === "Choose..." || $("#selectInOrOut").val() === "Please select an option") {
+            $("#selectInOrOut").val(" Please select an option");
+            return false;
+        } 
         var newMember = {
-            // user_name: $("#userName").val().trim(),
             password: $("#password").val().trim(),
             firstname: $("#firstName").val().trim(),
             lastname: $("#lastName").val().trim(),
             phone: $("#phone").val().trim(),
             email: $("#email").val().trim(),
             photoUrl: $("#photoUrl").val().trim(),
-            // member_frequency_preference: $("#selectFrequency option:selected").text(),
-            // member_inOrOut:  $("#selectInOrOut option:selected").text(),
+            member_frequency_preference: $("#selectFrequency option:selected").text(),
+            member_inOrOut:  $("#selectInOrOut option:selected").text(),
         };
         
-        // $('input[name="skills"]:checked').each(function() {   
-        //  newMember[this.value] = true;
-        //  });
+        $('input[name="skills"]:checked').each(function() {   
+         newMember[this.value] = true;
+         });
 
         console.log(newMember)
       
