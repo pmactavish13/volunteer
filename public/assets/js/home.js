20e32ee612ad40b2343048763edb59ed3b6d888d
Paula MacTavish <perilousp@hotmail.com>
2018-05-20 03:57:12 -0400

    form

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,4 +1,39 @@
-// Make sure to wait to attach handlers until the DOM is fully loaded.
 $(document).ready(function () {
 
+    $("#memberLoginForm").on("submit", function (event) {
+        event.preventDefault();
+        // if ($.trim($("#userName").val()) === "" || $.trim($("#userName").val()) === "User Name" || $.trim($("#userName").val()) === "Please enter a User Name") {
+        //     $("#userName").val(" Please enter a User Name");
+        //     return false;
+        // }
+        if ($.trim($("#email").val()) === "" || $.trim($("#email").val()) === "jDoe@email.com" || $.trim($("#email").val()) === "Please enter a valid e-mail Address") {
+            $("#email").val(" Please enter your e-mail Address");
+            return false;
+        }
+        if ($.trim($("#password").val()) === "" || $.trim($("#password").val()) === "Password" || $.trim($("#password").val()) === "Please enter a Password") {
+            $("#password").val(" Please enter a Password");
+            return false;
+        }
+        var login = {
+            password: $("#password").val().trim(),
+            email: $("#email").val().trim(),
+        }
+
+    //     // Send the POST request.
+    //     $.ajax("/api/login", {
+    //         type: "POST",
+    //         data: memberLogin
+    //     }).then(
+    //         function () {
+    //             // Reload the page to get the updated list
+    //             // location.reload();
+    //             $(location).attr('href', '/private')
+    //         }
+    //     );
+    });
+    
+    // if(window.location.href.indexOf('#myModal') != -1) {
+    //     $('#myModal').modal('show');
+    //   }
+    
 });
