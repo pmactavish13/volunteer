913754d6a2dbcdd5ee7db28e81fad145dfbd26e2
Paula MacTavish <perilousp@hotmail.com>
2018-05-22 20:01:22 -0400

    button

--- public/assets/js/private.js
+++ public/assets/js/private.js
@@ -12,4 +12,24 @@ $(document).ready(function () {
             }
         );
     });
+
+    // $(".register").on("click", function (event) {
+    //     var id = $(this).data("id");
+    //     event.preventDefault();
+
+    //     $.ajax("/api/register/" + id, {
+    //         type: "POST",
+    //     }).then(
+    //         function () {
+    //             // Reload the page to get the updated list
+    //             location.reload();
+    //         }
+    //     );
+    // });
+
+
+
+
+
+
 });
