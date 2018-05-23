246d826afbbd9d610f5d595bebf6e70e338224e0
Paula MacTavish <perilousp@hotmail.com>
2018-05-23 18:24:24 -0400

    tweak

--- public/assets/js/private.js
+++ public/assets/js/private.js
@@ -26,42 +26,4 @@ $(document).ready(function () {
         }
     });
 
-<<<<<<< HEAD
-    // $("#findAll").on("click", function (event) {
-    //     var id = $(this).data("id");
-    //     event.preventDefault();
-
-    //     $.ajax("/api/findAllMy/" + id, {
-    //         type: "GET",
-    //     }).then(
-    //         function () {
-    //             // Reload the page to get the updated list
-    //             location.reload();
-    //         }
-    //     );
-    // });
-
-    // $("#search").on("submit", function (event) {
-
-    //     var searchSkill = {};
-    //     event.preventDefault();
-
-    //     $('input[name="skillsSearch"]:checked').each(function () {
-    //         searchSkill[this.value] = true;
-    //     });
-
-    //     console.log(searchSkill);
-
-    //     $.ajax("/api/findAllMy", {
-    //         type: "POST",
-    //         data: searchSkill,
-    //         xhrFields: {
-    //             withCredentials: true
-    //         }
-    //     })
-
-
-    // })
-=======
->>>>>>> 30ada1008df039aafe4f2b01bb1103335bdfb6b3
 });
