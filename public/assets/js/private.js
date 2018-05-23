e3e17773ea61652519b8d87d47706a5e01348b06
Paula MacTavish <perilousp@hotmail.com>
2018-05-23 15:33:01 -0400

    my events nav bar and css on opportunity modal

--- public/assets/js/private.js
+++ public/assets/js/private.js
@@ -16,39 +16,4 @@ $(document).ready(function () {
         );
     });
 
-    // $("#findAll").on("click", function (event) {
-    //     var id = $(this).data("id");
-    //     event.preventDefault();
-
-    //     $.ajax("/api/findAllMy/" + id, {
-    //         type: "GET",
-    //     }).then(
-    //         function () {
-    //             // Reload the page to get the updated list
-    //             location.reload();
-    //         }
-    //     );
-    // });
-
-    // $("#search").on("submit", function (event) {
-    
-    //     var searchSkill = {};
-    //     event.preventDefault();
-        
-    //     $('input[name="skillsSearch"]:checked').each(function () {
-    //         searchSkill[this.value] = true;
-    //     });
-
-    //     console.log(searchSkill);
-
-    //     $.ajax("/api/findAllMy", {
-    //         type: "POST",
-    //         data: searchSkill,
-    //         xhrFields: {
-    //             withCredentials: true
-    //         }
-    //     })
-
-
-    // })
 });
